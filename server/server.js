7e500cc0109cf9fd9fcbdd136a6a05649181e74a
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-19 18:27:41 +0200

    feat: updated post method and ready to work

--- server/server.js
+++ server/server.js
@@ -54,14 +54,23 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 });
 
 // create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "mcdonalds", // mocked data
-    },
-  });
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+
+    console.log(results);
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // update a restaurant
