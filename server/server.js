f8137626361087cf96bc4e7815f52ccd68e63468
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-19 12:40:25 +0200

    feat: installed morgan to handle middlewares and implemented into server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 
 // get all restaurants
 app.get("/api/v1/restaurants", (req, res) => {
