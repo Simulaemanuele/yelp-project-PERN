ce02aab94be3ca4bdfbe5ac2724ed85e58bb92e9
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-19 11:40:13 +0200

    feat: created GET methods for all and one restaurant, a POST method to create one, installed cors package

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
-  console.log("Get all restaurants.");
+app.use(cors());
+
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["mcdonalds", "wendys"], // mocked data
+    },
+  });
+});
+
+// get a restaurant
+app.get("/api/v1/restaurants/:restaurantId", (req, res) => {
+  console.log(req.params);
+});
+
+// create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req);
 });
 
 const port = process.env.PORT || 3001;
