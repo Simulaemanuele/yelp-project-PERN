7051571c518a268a878cacb4a8ddcabe33253687
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-19 14:41:07 +0200

    feat + refact: refactored all restaurant route implmeneted and working fine

--- server/server.js
+++ server/server.js
@@ -11,15 +11,20 @@ app.use(express.json());
 
 // get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-  const results = await db.query("SELECT * FROM restaurants;");
-  console.log(results);
+  try {
+    const results = await db.query("SELECT * FROM restaurants;");
+    console.log(results);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: ["mcdonalds", "wendys"], // mocked data
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // get a restaurant
