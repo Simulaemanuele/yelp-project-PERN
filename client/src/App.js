a497e4cc6d5e747929d405bbc4413bb9d6159220
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-20 16:49:54 +0200

    feat: created context folder and component implemented in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import UpdatePage from "./routes/UpdatePage";
+import { RestaurantContextProvider } from "./context/RestaurantsContext";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantContextProvider>
   );
 }
 
---
+++ client/src/context/RestaurantsContext.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, createContext } from "react";
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantContextProvider = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  return (
+    <RestaurantsContext.Provider value={{ restaurants, setRestaurants }}>
+      {props.children}
+    </RestaurantsContext.Provider>
+  );
+};
