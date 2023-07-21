15fea39c9f34e29badf55778e95c44952974ad62
Emanuele Simula <simulaemanuele@gmail.com>
2023-07-21 15:13:00 +0200

    feat: created StarRating component that is working fine and implemented in RestaurantDetailPage

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const StarRating = ({ rating }) => {
   const stars = [];
-  return <div></div>;
+
+  for (let i = 1; i <= 5; i++) {
+    i <= rating
+      ? stars.push(<i className="fas fa-star"></i>)
+      : i === Math.ceil(rating) && !Number.isInteger(rating)
+      ? stars.push(<i className="fas fa-star-half-alt"></i>)
+      : stars.push(<i className="far fa-star"></i>);
+  }
+
+  return <>{stars}</>;
 };
 
 export default StarRating;
--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import StarRating from "../components/StarRating";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -21,7 +22,7 @@ const RestaurantDetailPage = () => {
     fetchData();
   }, [id, setSelectedRestaurant]);
 
-  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+  return <div>{selectedRestaurant && <StarRating rating={3.2} />}</div>;
 };
 
 export default RestaurantDetailPage;
