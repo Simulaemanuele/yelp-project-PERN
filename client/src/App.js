1ab51fdfcf0a72052ec5a9bd395ab91a76f10a8f
Emanuele Simula <simulaemanuele@gmail.com>
2023-08-20 13:54:38 +0200

    feat: created Login component and added to the route system in App. Next create a DB that can record of email and passwords

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,21 @@ import Home from "./routes/Home";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import UpdatePage from "./routes/UpdatePage";
 import { RestaurantContextProvider } from "./context/RestaurantsContext";
+import Login from "./components/Login";
 
 function App() {
   return (
     <RestaurantContextProvider>
-      <div className="container">
+      <div className="container-fluid">
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+            <Route
+              path="/home/restaurants/:id"
+              element={<RestaurantDetailPage />}
+            />
           </Routes>
         </Router>
       </div>
